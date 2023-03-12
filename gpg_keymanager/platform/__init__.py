fa9f6e7c5d25e02af490b19f5fab434b32248463
Ilkka Tuohela <hile@iki.fi>
2023-03-12 09:26:13 +0200

    Remove unused gpg_keymanager.platform module

--- gpg_keymanager/platform/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (C) 2020-2023 by Ilkka Tuohela <hile@iki.fi>
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-"""
-Platform specific implementations of various platform specific tasks
-"""
--- tests/platform/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (C) 2020-2023 by Ilkka Tuohela <hile@iki.fi>
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-"""
-Unit tests for gpg_keymanager.platform module
-"""
